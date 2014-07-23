b9459e9784a68b9ebbaa7ddf75291c80b37a9b05
Max Kohl <maxkohl810@gmail.com>
2014-07-22 21:53:20 -0400

    create a todo scaffold

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,17 +1,6 @@
 class HomeController < ApplicationController
 
-  respond_to :json
-
-  def index
-    @todos = ToDo.all
-
-    respond_with(@todos)
-  end
-
   def show
-    @todo = ToDo.find(params[:id])
-
-    respond_with(@todo)
   end
 
 end
